1f578512a160606d8f7b97ac1a43cc0d4f900363
felipeghidini <lipeghidini18@gmail.com>
2021-10-10 06:44:29 -0300

    Criado pasta backend para fakeApi e alterado a pasta principal

---
+++ agendaAula/agenda-aula-front/src/app/views/home/aula-form-dialog/aula-form-dialog.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-aula-form-dialog',
+  templateUrl: './aula-form-dialog.component.html',
+  styleUrls: ['./aula-form-dialog.component.css']
+})
+export class AulaFormDialogComponent implements OnInit {
+  public aulaForm: FormGroup;
+
+  constructor(
+    public dialogRef: MatDialogRef<AulaFormDialogComponent>,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.aulaForm = this.fb.group({
+      sala: ['', [Validators.required]],
+      nome: ['', [Validators.required]],
+      aula: ['', [Validators.required]],
+      data: ['', [Validators.required]],
+      inicioAula: ['', [Validators.required]],
+      fimAula: ['', [Validators.required]]
+    });
+  }
+
+  createAula() {
+    //let newDate: moment.Moment = moment.utc(this.aulaForm.value.data).local();
+    //this.aulaForm.value.data = newDate.format("YYYY-MM-DD");
+    this.dialogRef.close();
+    this.aulaForm.reset();
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+    this.aulaForm.reset();
+  }
+
+}
